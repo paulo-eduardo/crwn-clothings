a31d2e68d8e9137a2d2b5a26a3ad5968a23be59c
Paulo Eduardo <paulo.es@live.com>
2019-07-12 09:55:08 -0300

    config eslint

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 import "./sign-in.styles.scss";
 
@@ -50,7 +51,9 @@ export default class SignIn extends Component {
             required
           />
 
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit" value="Submit Form">
+            Sign in
+          </CustomButton>
         </form>
       </div>
     );
