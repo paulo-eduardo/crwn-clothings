fd549e17002e67ada049a9f7ed87216351968a88
Paulo Silva <paulo.es@live.com>
2019-08-06 23:21:50 -0300

    changing data format for faster index for type

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,11 +8,15 @@ import { selectCollection } from '../../redux/shop/shop.selectors'
 import './collection.styles.scss'
 
 const CollectionPage = ({ match, collection }) => {
-  console.log('teste1', match)
-  console.log('teste', collection)
+  const { title, items } = collection
   return (
     <div className="collection-page">
-      <h2>COLLECTIONb PAGE</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   )
 }
