353fd8fba749d3e032472e71130faa6ab215c8db
Paulo Silva <paulo.es@live.com>
2019-08-26 22:37:35 -0300

    changed to use styles

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import CustomButton from '../custom-button/custom-button.component'
 import { addItem } from '../../redux/cart/cart.actions'
 
 import './collection-item.styles.scss'
+import { CustomButtonStyled } from './collection-item.styles'
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item
@@ -20,9 +20,9 @@ const CollectionItem = ({ item, addItem }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
+      <CustomButtonStyled onClick={() => addItem(item)} inverted>
         Add to cart
-      </CustomButton>
+      </CustomButtonStyled>
     </div>
   )
 }
